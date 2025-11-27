4a6e6d0b1a7e7a7d2c8c746303eb96bdeb4b1602
matosys <max@matosys.de>
2025-11-27 15:25:43 +0100

    docs(scenario_features): add basic docstrings to classes and methods

--- src/balderhub/crud/lib/scenario_features/basic_interactor_feature.py
+++ src/balderhub/crud/lib/scenario_features/basic_interactor_feature.py
@@ -3,9 +3,20 @@ from balderhub.data.lib.utils.response_message_list import ResponseMessageList
 
 
 class BasicInteractorFeature(AbstractDataItemRelatedFeature):
+    """
+    Feature class that interacts with the system-under-test in some kind.
+    """
 
     def get_active_success_messages(self) -> ResponseMessageList:
+        """
+        :return: returns a list of all active success messages
+        """
+        # TODO do we need an observer that listens to all kind of messages during interaction
         raise NotImplementedError()
 
     def get_active_error_messages(self) -> ResponseMessageList:
+        """
+        :return: returns a list of all active error messages
+        """
+        # TODO do we need an observer that listens to all kind of messages during interaction
         raise NotImplementedError()
--- src/balderhub/crud/lib/scenario_features/single_data_deleter_feature.py
+++ src/balderhub/crud/lib/scenario_features/single_data_deleter_feature.py
@@ -3,9 +3,19 @@ from balderhub.crud.lib.scenario_features.basic_interactor_feature import BasicI
 
 
 class SingleDataDeleterFeature(BasicInteractorFeature):
+    """
+    Scenario Feature that deletes an existing data item in the system-under-test.
+    """
+    # TODO improve - different types of deleting
 
     def load(self):
+        """
+        Loads the system-under-test to be in the state for deleting the data item.
+        """
         raise NotImplementedError
 
-    def delete(self):
+    def delete(self) -> None:
+        """
+        Executes the deleting of the data item.
+        """
         raise NotImplementedError()
