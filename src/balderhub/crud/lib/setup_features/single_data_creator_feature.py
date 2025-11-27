1263e320f3f5eafeee36860f65cda78cb3fb08f2
matosys <max@matosys.de>
2025-11-27 15:25:54 +0100

    docs(setup_features): add basic docstrings to classes and methods

--- src/balderhub/crud/lib/setup_features/single_data_creator_feature.py
+++ src/balderhub/crud/lib/setup_features/single_data_creator_feature.py
@@ -10,6 +10,9 @@ ElementContainerTypeT = TypeVar('ElementContainerTypeT')
 
 
 class SingleDataCreatorFeature(scenario_features.SingleDataCreatorFeature):
+    """
+    Setup creating feature with field callback mapping
+    """
 
     def item_mapping(self) -> Dict[str, FillItemmappingCallback]:
         """returns a dictionary with the dataclass field name as key and its configuration as value"""
