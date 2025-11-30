ca743981c722d1ec88ddeb993c113cf9bc9e8165
matosys <max@matosys.de>
2025-11-30 13:46:03 +0100

    feat(field-value-provider): remove dataitem decorator
    in balderhub-data>=0.0.1b3 native dataclasses are not used anymore - pydantic is used now

--- tests/lib/utils/data_items/author_data_item.py
+++ tests/lib/utils/data_items/author_data_item.py
@@ -1,9 +1,6 @@
-import balderhub.data.lib.utils.decorator_dataitem
-
 from balderhub.data.lib.utils import SingleDataItem
 
 
-@balderhub.data.lib.utils.decorator_dataitem.dataitem
 class AuthorDataItem(SingleDataItem):
     id: int
     first_name: str
--- tests/lib/utils/data_items/book_data_item.py
+++ tests/lib/utils/data_items/book_data_item.py
@@ -1,11 +1,8 @@
-import balderhub.data.lib.utils.decorator_dataitem
-
 from balderhub.data.lib.utils import SingleDataItem
 from tests.lib.utils.data_items.author_data_item import AuthorDataItem
 from tests.lib.utils.data_items.book_category_data_item import BookCategoryDataItem
 
 
-@balderhub.data.lib.utils.decorator_dataitem.dataitem
 class BookDataItem(SingleDataItem):
     id: int
     title: str
